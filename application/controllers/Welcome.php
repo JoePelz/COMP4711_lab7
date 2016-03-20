079a54976efa924f7563574da0c2badf6a5f704e
Brian Livesey <bri@brianlivesey.com>
2016-03-19 23:30:23 -0700

    Homepage display, seaching, and Bingo

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -18,13 +18,43 @@ class Welcome extends CI_Controller {
      * map to /index.php/welcome/<method_name>
      * @see https://codeigniter.com/user_guide/general/urls.html
      */
+	 
+	public function __construct() {
+		parent::__construct();
+		$this->load->model('timetable');
+		$this->load->helper('form');
+	}
+	
     public function index()
     {   
-        $this->load->model('timetable');
+		$bingo = false;
+		
+		if ($this->input->post('searchbutton') != null) {
+			$this->data['searchresult'] = "<h2>Search Result</h2>\n";
+			$search = true;
+			$days_search = $this->input->post('days');
+			$courses_search = $this->input->post('courses');
+			$slots_search = $this->input->post('slots');
+			
+			$times = (array)$this->timetable->get_bookings_by_time($slots_search);
+			$days = (array)$this->timetable->get_bookings_by_day($days_search);
+			$courses = (array)$this->timetable->get_bookings_by_course($courses_search);
+			
+			// Check for search 'bingo' in a long, protracted, drawn-out and ugly way
+			if (sizeof($courses[$courses_search]) == 1 && sizeof($times[$slots_search]) == 1 
+				&& sizeof($days[$days_search]) == 1
+				&& $courses[$courses_search][0]->course == $times[$slots_search][0]->course
+				&& $courses[$courses_search][0]->course == $days[$days_search][0]->course) {
+					// Well hot-damn ma; we gots us a bingo on our hands
+					$bingo = true;
+				}
+		} else {
         //times is an array of arrays of Bookings objects
-        $times = (array)$this->timetable->get_bookings_by_time();
-        $days = (array)$this->timetable->get_bookings_by_day();
-        $courses = (array)$this->timetable->get_bookings_by_course();
+			$times = (array)$this->timetable->get_bookings_by_time(null);
+			$days = (array)$this->timetable->get_bookings_by_day(null);
+			$courses = (array)$this->timetable->get_bookings_by_course(null);
+			$this->data['searchresult'] = "";
+		}
         
         $timeFacet = array();
         foreach($times as $time => $classes) {
@@ -62,7 +92,26 @@ class Welcome extends CI_Controller {
         }
         $this->data['courses'] = $courseFacet;
         
-        $this->parser->parse('welcome', $this->data);
-        
+		
+		// Display form
+		$searchform = form_open('welcome');
+		$searchform .= form_dropdown('days', $this->timetable->get_days());
+		$searchform .= form_dropdown('courses', $this->timetable->get_courses());
+		$searchform .= form_dropdown('slots', $this->timetable->get_timeslots());
+		$searchform .= form_submit('searchbutton', 'Search');
+		$searchform .= form_close();
+		
+		$this->data['searchform'] = $searchform;
+		
+		if ($bingo) {
+			$this->data['searchresult'] .= "<div class = 'bingo'>BINGO</div><br>\n";
+			$this->data['searchresult'] .= $timeFacet[0]['time'];
+		}
+		
+		$this->parser->parse('welcome', $this->data);
     }
+	
+	public function search() { // $course, $day, $slot
+		echo "holler";
+	}
 }
